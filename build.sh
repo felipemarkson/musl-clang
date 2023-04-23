dc617798eccb531677c11ff36ac6ce128ca63f74
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2023-04-23 19:04:50 -0300

    Fixing minor typo

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 FILE_NO_EXTENSION='main'
 
 export PATH=$PATH:./musl-dist/bin
-rm -f main
+rm -f $FILE_NO_EXTENSION
 
 libc="musl-dist"
-libc_inc="musl-dist/include"
-libc_lib="musl-dist/lib"
+libc_inc=$libc"/include"
+libc_lib=$libc"/lib"
 MUSL_FLAGS="-fuse-ld=lld -static -nostdinc --sysroot $libc -isystem $libc_inc -L-user-start -L $libc_lib -L-user-end"
 
 clang $MUSL_FLAGS -o $FILE_NO_EXTENSION $FILE_NO_EXTENSION.c
