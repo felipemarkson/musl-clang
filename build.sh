1e405209242032ef06bac46482ce637ed1bd2ea9
Felipe Markson dos Santos Monteiro <47871564+felipemarkson@users.noreply.github.com>
2023-04-23 18:58:50 -0300

    Fixing usage of system's musl libraries.

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ FILE_NO_EXTENSION='main'
 export PATH=$PATH:./musl-dist/bin
 rm -f main
 
-musl-clang -o $FILE_NO_EXTENSION $FILE_NO_EXTENSION.c
+libc="musl-dist"
+libc_inc="musl-dist/include"
+libc_lib="musl-dist/lib"
+MUSL_FLAGS="-fuse-ld=lld -static -nostdinc --sysroot $libc -isystem $libc_inc -L-user-start -L $libc_lib -L-user-end"
 
+clang $MUSL_FLAGS -o $FILE_NO_EXTENSION $FILE_NO_EXTENSION.c
